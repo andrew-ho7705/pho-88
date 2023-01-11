77bba02163200560250a951ff517b64f9459ec99
Andrew Ho <amh7705@psu.edu>
2023-01-10 21:29:17 -0500

    Progress

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const appetizerContainer = document.getElementsByClassName('.appetizers');
 
-fetch('./menuItems.json')
+function displayAppetizers() {
+  fetch('../menuItems.json')
     .then(response => response.json())
     .then(items => {
-        
+        items.Appetizers.forEach(item => {
+          let text = document.createElement('p');
+          text = item.Appetizers.id;
+          appetizerContainer.appendChild(text);
+        });
     });
+}
