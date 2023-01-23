c51bbc043af183729e46b0076f372e5e2b32a893
Andrew Ho <amh7705@psu.edu>
2023-01-23 01:21:11 -0500

    Adding some menu items

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const appetizerContainer = document.getElementsByClassName('.appetizers');
 
 function displayAppetizers() {
-  fetch('../menuItems.json')
-    .then(response => response.json())
-    .then(items => {
-        items.Appetizers.forEach(item => {
-          let text = document.createElement('p');
-          text = item.Appetizers.id;
-          appetizerContainer.appendChild(text);
-        });
-    });
+  console.log("hi");
+  // fetch('../menuItems.json')
+  //   .then(response => response.json())
+  //   .then(items => {
+  //       items.Appetizers.forEach(item => {
+  //         let text = document.createElement('p');
+  //         text = item.Appetizers.id;
+  //         appetizerContainer.appendChild(text);
+  //       });
+  //   });
 }
